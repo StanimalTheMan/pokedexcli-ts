b4b03b0616b58f54babf3de290e5fec3c3839185
Stan Choi <sjc591@nyu.edu>
2025-07-27 14:57:16 -0400

    Init REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,25 @@
+import { exit } from "process";
+import { createInterface } from "readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Pokedex > ",
+});
+
+export function startREPL() {
+  rl.prompt();
+  rl.on("line", (input: string) => {
+    if (input.length == 0) {
+      rl.prompt();
+    } else {
+      const cleanedInput = cleanInput(input);
+      console.log(`Your command was: ${cleanedInput[0]}`);
+      rl.prompt();
+    }
+  });
+}
+
 export function cleanInput(input: string): string[] {
   // remove excessive whitespace
   const cleanedInput = input.replace(/\s+/g, " ").trim().toLowerCase();
