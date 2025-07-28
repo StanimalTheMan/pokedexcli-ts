47a04a4c28c6d0740ec5df0e31898e4c8f8d6eba
Stan Choi <sjc591@nyu.edu>
2025-07-28 09:22:19 -0400

    Add help and exit

--- src/repl.ts
+++ src/repl.ts
@@ -1,5 +1,5 @@
-import { exit } from "process";
 import { createInterface } from "readline";
+import { getCommands } from "./command.js";
 
 const rl = createInterface({
   input: process.stdin,
@@ -14,7 +14,13 @@ export function startREPL() {
       rl.prompt();
     } else {
       const cleanedInput = cleanInput(input);
-      console.log(`Your command was: ${cleanedInput[0]}`);
+      //   console.log(`Your command was: ${cleanedInput[0]}`);
+      const commands = getCommands();
+      if (cleanedInput[0] in commands) {
+        commands[cleanedInput[0]]["callback"](commands);
+      } else {
+        console.log("Unknown command");
+      }
       rl.prompt();
     }
   });
